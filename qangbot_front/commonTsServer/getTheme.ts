ca80318e8ef7960751da9f1d080c1d63acbd1ad0
4tendev <4tenprog@gmail.com>
2024-03-27 14:13:13 +0330

    fixed with new type theme

--- qangbot_front/commonTsServer/getTheme.ts
+++ qangbot_front/commonTsServer/getTheme.ts
@@ -1,12 +1,10 @@
 import { cookies } from "next/headers";
-import config from "@/tailwind.config";
+import config ,{Theme} from "@/tailwind.config";
 import {THEME_COOKIE_NAME} from "@/settings"
 
-type Theme = typeof config.daisyui.themes[number];
 
 const getTheme = (): Theme => {
-  const cookieStore =   cookies()
-  const theme = cookieStore.get(THEME_COOKIE_NAME)?.value
+  const theme =  cookies().get(THEME_COOKIE_NAME)?.value
   return config.daisyui.themes.includes(theme)
     ? theme
     : config.daisyui.themes[0]
