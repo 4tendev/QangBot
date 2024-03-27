129ca1e012c5948ebe66d84c836b65dc3731fed0
4tendev <4tenprog@gmail.com>
2024-03-27 14:14:19 +0330

    fix hieght in all browser

---
+++ qangbot_front/app/Main.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { Navbar_Height } from '@/settings';
+import React, { useEffect } from 'react';
+
+function Main({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        const mainElement :HTMLElement | null = document.querySelector('.mainContent');
+        if (mainElement) {
+          mainElement.style.height = (window.innerHeight - Navbar_Height) + 'px';
+        }
+    
+        function handleResize() {
+          if (mainElement) {
+            mainElement.style.height = (window.innerHeight - Navbar_Height) + 'px';
+          }
+        }
+    
+        window.addEventListener('resize', handleResize);
+        
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+  return (
+    <main className="w-full  mainContent overflow-auto bg-base-100">
+      {children}
+    </main>
+  );
+}
+
+export default Main;
