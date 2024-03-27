82643498c2695952cffa408504c5ca877735725a
4tendev <4tenprog@gmail.com>
2024-03-27 14:12:16 +0330

    fix settings with language object

--- qangbot_front/settings.ts
+++ qangbot_front/settings.ts
@@ -1,7 +1,9 @@
-export const SUPPORTED_LANGUAGES :  ("en"|"fa")[] = ["en", "fa"] 
-export const LANGUAGES_COOKIE_NAME :"decidedLanguage" = "decidedLanguage" 
-export const SUPPORTED_DIR :("ltr" | "rtl")[] = ["ltr", "rtl"] 
+export const LANGUAGES_COOKIE_NAME :"lang" = "lang" 
 export const THEME_COOKIE_NAME :"theme" = "theme" 
+export const Navbar_Height :number = 80 
+export type Language = "en"|"fa"
+export type Direction = "ltr" | "rtl"
+export const SUPPORTED_LANGUAGES :  {lang :Language , dir : Direction ,text : string}[] = [{lang:"en",dir:"ltr",text:"English"}, {lang:"fa",dir:"rtl",text:"فارسی"}] 
 
 
 
