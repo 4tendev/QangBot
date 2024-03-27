b5c3d0edf931aa76063fb68f0080238c0ce20c75
4tendev <4tenprog@gmail.com>
2024-03-27 14:14:34 +0330

    Create menu.ts

---
+++ qangbot_front/app/navbar/menu.ts
@@ -0,0 +1,17 @@
+type Name =  "gridbot" | "strategy" | "aboutus"
+type Link = string
+const menu : {name :Name , link :Link}[] =[
+    {
+        "name" : "gridbot" , "link" : "/gridbot"
+    },
+    {
+        "name" : "strategy" , "link" : "/strategy"
+    },
+    {
+        "name" : "aboutus" , "link" : "/aboutus"
+    }
+    
+    
+] 
+
+export default menu
