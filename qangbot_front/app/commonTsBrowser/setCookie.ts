4bb326377a715ffbe3f538099d46c789c4c0dd38
4tendev <4tenprog@gmail.com>
2024-03-27 14:11:45 +0330

    setCookie

---
+++ qangbot_front/app/commonTsBrowser/setCookie.ts
@@ -0,0 +1,7 @@
+
+export default function setCookie(name :string, value : string, expirationDays :number) {
+    const date = new Date();
+    date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
