9c03f234ca6ebc34af14f0a667ed9fa53fefe674
4tendev <4tenprog@gmail.com>
2024-04-03 11:42:23 +0330

    Create languageSlice.ts

---
+++ qangbot_front/GlobalStates/Slices/languageSlice.ts
@@ -0,0 +1,29 @@
+import type { PayloadAction  } from "@reduxjs/toolkit";
+import  { createSlice } from "@reduxjs/toolkit";
+import { SUPPORTED_LANGUAGES } from "@/settings";
+
+export type LanguageSliceState = typeof SUPPORTED_LANGUAGES[number]
+
+const initialState:  LanguageSliceState = SUPPORTED_LANGUAGES[0]
+
+export const languageSlice = createSlice({
+  name: "language",
+  initialState,
+  reducers: (create) => ({
+
+    selectedLanguage: create.reducer(
+      (state, action: PayloadAction<LanguageSliceState>) =>  action.payload
+    ),
+
+    
+  }),
+
+  selectors: {
+    language: (language) => language
+  },
+});
+
+export const { selectedLanguage } =
+languageSlice.actions;
+
+export const { language } = languageSlice.selectors;
