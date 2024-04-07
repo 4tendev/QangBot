7903ad4cc286352c8e76d44394c52e1b98f14574
4tendev <4tenprog@gmail.com>
2024-04-07 15:53:20 +0330

    user section

---
+++ qangbot_front/app/user/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React from 'react'
+import UserProfile from '../navbar/Profile/UserProfile/UserProfile'
+
+const Page = () => {
+  return (
+    <UserProfile />
+  )
+}
+
+export default Page
