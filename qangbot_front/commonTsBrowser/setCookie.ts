e01a4aecd65c08a225c9a9dc0d55d5e3a414bc67
4tendev <4tenprog@gmail.com>
2024-04-08 16:27:23 +0330

    set coockie days optional

--- qangbot_front/commonTsBrowser/setCookie.ts
+++ qangbot_front/commonTsBrowser/setCookie.ts
@@ -1,7 +1,7 @@
 
-export default function setCookie(name :string, value : string, expirationDays :number) {
+export default function setCookie(name :string, value : string, expirationDays :number =0) {
     const date = new Date();
     date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
+    const expires = expirationDays > 0 ? "expires=" + date.toUTCString() :"";
     document.cookie = `${name}=${value}; ${expires}; path=/`;
 }
