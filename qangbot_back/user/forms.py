9b0fa2617f4ae5cdb6d7136309c916f44b286496
4tendev <4tenprog@gmail.com>
2024-04-08 12:04:38 +0330

    add trusdtedDevice Field

--- qangbot_back/user/forms.py
+++ qangbot_back/user/forms.py
@@ -1,18 +1,22 @@
 from django import forms
 
 
-emailCodeField=emailCode=forms.CharField(required=False) 
+emailCodeField=forms.CharField(required=False) 
+trustedDviceField=forms.BooleanField( required=False)
 
 class LoginForm(forms.Form):
     username=forms.CharField(max_length=50, required=True)
     password=forms.CharField(max_length=100,required=True)
     TOTPCode=forms.CharField(required=False)
+    trustedDvice=trustedDviceField
 
 class RegisterForm(forms.Form):
     username=forms.CharField(max_length=50,min_length=6, required=True)
     password=forms.CharField(max_length=100,min_length=8,required=True)
     email=forms.EmailField(required=True)
     emailCode=emailCodeField
+    trustedDvice=trustedDviceField
+
 class ResetPasswordForm(forms.Form):
     email=forms.EmailField(required=True)
     newPassword=forms.CharField(max_length=100,min_length=8,required=False)
