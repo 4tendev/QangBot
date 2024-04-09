3d20b90b03dd779e954d765db24b851e45308f31
4tendev <4tenprog@gmail.com>
2024-04-09 15:36:56 +0330

    remove main login test

--- qangbot_front/app/page.tsx
+++ qangbot_front/app/page.tsx
@@ -1,23 +1,12 @@
-"use client"
-import { isKnown } from "@/GlobalStates/Slices/userSlice";
-import {  useAppSelector ,useAppDispatch } from "@/GlobalStates/hooks";
-import {
-  newState,
 
-} from "@/GlobalStates/Slices/userSlice";
 
 
 export default function Home() {
-  const userisKnown = useAppSelector(isKnown);
-  const setUserIsKnow = useAppDispatch();
+
   return (
     <main className="">
-          {userisKnown ? "loged" : "NOTloged"}
-          loged
-          <br />
-          <button onClick={() => setUserIsKnow(newState(true))}>login</button>
-          <br />
-          <button onClick={() => setUserIsKnow(newState(false))}>logout</button>
+      Main
+
     </main>
   );
 }
