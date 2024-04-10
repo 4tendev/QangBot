f3c655810e775374dc36fb8c7a44140e8a44f441
4tendev <4tenprog@gmail.com>
2024-04-10 13:32:10 +0330

    centered

--- qangbot_front/app/loading.tsx
+++ qangbot_front/app/loading.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
   return (
-    <span className="loading loading-bars loading-lg mt-10"></span>
-  )
-}
+    <div className="w-full flex justify-center">
+      <span className="loading loading-bars loading-lg mt-10 "></span>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
