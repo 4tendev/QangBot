7dc2ee9b5b538d91f2fb5d4ccd9cacc6a5a358bc
4tendev <4tenprog@gmail.com>
2024-04-10 16:17:23 +0330

    Create GlobalAlert.tsx

---
+++ qangbot_front/app/GlobalAlert.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react'
+import TimeAlert from './components/TimeAlert/TimeAlert'
+import { useAppSelector } from "@/GlobalStates/hooks";
+import { globalalert } from "@/GlobalStates/Slices/alertSlice";
+
+
+const GlobalAlert = () => {
+  const newAlert = useAppSelector(globalalert)
+  return (
+    newAlert.message ?  
+    <TimeAlert key={Math.random()} timeAlert={newAlert} /> 
+    :null
+  )
+}
+
+export default GlobalAlert
