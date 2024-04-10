a0b0a162c3bc947196e651eb573a98767e3d3906
4tendev <4tenprog@gmail.com>
2024-04-10 17:29:53 +0330

    only type

--- qangbot_front/commonTsBrowser/fetchAPI.ts
+++ qangbot_front/commonTsBrowser/fetchAPI.ts
@@ -1,20 +1,21 @@
-
 import { NEXT_PUBLIC_BACKEND_URL } from "@/settings";
-export function fetchapi(endpoint : string , method : "GET" | "POST" |"PATCH" |"PUT" | "DELETE" , data? :object) {
-    return fetch(NEXT_PUBLIC_BACKEND_URL + endpoint, {
-      method: method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      
-      body: data?   JSON.stringify(data) : null,
-      credentials: 'include',
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      });
+export function fetchapi(
+  endpoint: string,
+  method: "GET" | "POST" | "PATCH" | "PUT" | "DELETE",
+  data?: object
+) {
+  return fetch(NEXT_PUBLIC_BACKEND_URL + endpoint, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
 
+    body: data ? JSON.stringify(data) : null,
+    credentials: "include",
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  });
 }
