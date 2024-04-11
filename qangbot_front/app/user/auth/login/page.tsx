fa9c50c67edc16bb426e6aab814fa53a386be038
4tendev <4tenprog@gmail.com>
2024-04-11 11:20:31 +0330

    fix language

--- qangbot_front/app/user/auth/login/page.tsx
+++ qangbot_front/app/user/auth/login/page.tsx
@@ -8,13 +8,18 @@ import React from "react";
 
 import { useRouter, usePathname } from "next/navigation";
 import { fetchapi } from "@/commonTsBrowser/fetchAPI";
-import { useAppDispatch } from "@/GlobalStates/hooks";
+import { useAppDispatch, useAppSelector } from "@/GlobalStates/hooks";
 import { newUserState } from "@/GlobalStates/Slices/userSlice";
-import { newAlert } from "@/GlobalStates/Slices/alert/Slice";
+import {
+  newAlert,
+  serverErrorAlert,
+  connectionErrorAlert,
+} from "@/GlobalStates/Slices/alert/Slice";
+import { language } from "@/GlobalStates/Slices/languageSlice";
 
 const Page = () => {
   const setGlobalState = useAppDispatch();
-  const lang = "en";
+  const lang = useAppSelector(language).lang;
   const router = useRouter();
   const pathname = usePathname();
   const form = {
@@ -22,36 +27,48 @@ const Page = () => {
     action: dictionary.login[lang],
   };
   async function login(data: any) {
-    await fetchapi("/user/", "PATCH", (data = data)).then((response) => {
-      switch (response.code) {
-        case "200":
-          setGlobalState(newUserState(true));
-          setGlobalState(newAlert({ mode: "success", message:  dictionary.code200[lang], time: 3 }));
-          pathname === "/user/auth" ? router.push("/user") : null;
-          break;
-        case "400":
-          setGlobalState(
-            newAlert({
-              mode: "warning",
-              message: dictionary.code400[lang],
-              time: 3,
-            })
-          );
-          break;
-        case "429":
-          setGlobalState(
-            newAlert({
-              mode: "warning",
-              message: dictionary.code429[lang],
-              time: 3,
-            })
-          );
-          break;
-
-        default:
-          break;
-      }
-    });
+    await fetchapi("/user/", "PATCH", (data = data))
+      .then((response) => {
+        switch (response.code) {
+          case "200":
+            setGlobalState(newUserState(true));
+            setGlobalState(
+              newAlert({
+                mode: "success",
+                message: dictionary.code200[lang],
+                time: 3,
+              })
+            );
+            pathname === "/user/auth" ? router.push("/user") : null;
+            break;
+          case "400":
+            setGlobalState(
+              newAlert({
+                mode: "warning",
+                message: dictionary.code400[lang],
+                time: 4,
+              })
+            );
+            break;
+          case "429":
+            setGlobalState(
+              newAlert({
+                mode: "warning",
+                message: dictionary.code429[lang],
+                time: 4,
+              })
+            );
+            break;
+          case "500":
+            setGlobalState(serverErrorAlert(lang));
+            break;
+          default:
+            break;
+        }
+      })
+      .catch((resson) => {
+        setGlobalState(connectionErrorAlert(lang));
+      });
   }
   return (
     <div className="mx-auto max-w-md px-3">
