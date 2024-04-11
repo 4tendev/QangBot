d0ed63faac315c291edc62251dc0bf0bd756be54
4tendev <4tenprog@gmail.com>
2024-04-11 11:06:09 +0330

    login added front

--- qangbot_front/app/user/auth/login/page.tsx
+++ qangbot_front/app/user/auth/login/page.tsx
@@ -1,22 +1,61 @@
 "use client";
+
 import UseFormTemplate from "@/app/UseForm/UseFormTemplate";
 
 import { passwordInput, usernameInput } from "../inputs/inputs";
 import dictionary from "./dictionary.json";
 import React from "react";
 
+import { useRouter, usePathname } from "next/navigation";
+import { fetchapi } from "@/commonTsBrowser/fetchAPI";
+import { useAppDispatch } from "@/GlobalStates/hooks";
+import { newUserState } from "@/GlobalStates/Slices/userSlice";
+import { newAlert } from "@/GlobalStates/Slices/alert/Slice";
+
 const Page = () => {
+  const setGlobalState = useAppDispatch();
   const lang = "en";
+  const router = useRouter();
+  const pathname = usePathname();
   const form = {
     inputs: [usernameInput(lang), passwordInput(lang)],
     action: dictionary.login[lang],
   };
-  async function action(data: any) {
-    return <div>test</div>;
+  async function login(data: any) {
+    await fetchapi("/user/", "PATCH", (data = data)).then((response) => {
+      switch (response.code) {
+        case "200":
+          setGlobalState(newUserState(true));
+          setGlobalState(newAlert({ mode: "success", message: "OK", time: 3 }));
+          pathname === "/user/auth" ? router.push("/user") : null;
+          break;
+        case "400":
+          setGlobalState(
+            newAlert({
+              mode: "warning",
+              message: dictionary.code400[lang],
+              time: 3,
+            })
+          );
+          break;
+        case "429":
+          setGlobalState(
+            newAlert({
+              mode: "warning",
+              message: dictionary.code429[lang],
+              time: 3,
+            })
+          );
+          break;
+
+        default:
+          break;
+      }
+    });
   }
   return (
     <div className="mx-auto max-w-md px-3">
-      <UseFormTemplate form={form} action={action} />
+      <UseFormTemplate form={form} action={login} />
     </div>
   );
 };
