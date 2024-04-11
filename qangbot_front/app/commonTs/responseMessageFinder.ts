81081d09dbf487d9b594c2f681cec33be1b1ccb9
4tendev <4tenprog@gmail.com>
2024-04-11 15:34:52 +0330

    type

--- qangbot_front/app/commonTs/responseMessageFinder.ts
+++ qangbot_front/app/commonTs/responseMessageFinder.ts
@@ -1,22 +1,18 @@
 import { Language } from "@/settings";
-import dictionary from "./dictionary.json"
-export default function responseMessage(selectedDictionary:{[key: string]: {[key: string] : string }} , lang : Language , code:string ) :string {
-
-    switch (code) {
-        case "500":
-            return  dictionary["500"][lang]
-        default:
-            break;
-    }
-    try {
-        const response = selectedDictionary[code][lang]
-        return response
-        
-    } catch (error) {
-
-        return  dictionary.unknownCode[lang]
-        
-    }
-   
-    
+import dictionary from "./dictionary.json";
+export default function responseMessage(
+  selectedDictionary: { [key: string]: { [key: string]: string } },
+  lang: Language,
+  code: string
+): string {
+  switch (code) {
+    case "500":
+      return dictionary["500"][lang];
+  }
+  try {
+    const response = selectedDictionary[code][lang];
+    return response;
+  } catch (error) {
+    return dictionary.unknownCode[lang];
+  }
 }
