fa31957e5aa8b21e7fd004ab5384ba3c4358a1d5
4tendev <4tenprog@gmail.com>
2024-04-30 19:06:14 +0330

    bot script

---
+++ qangbot_back/gridbot/management/commands/gridbots.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand
+import time
+import redis
+from gridbot.models import GridBot
+from core.settings import REDIS_URL
+
+redis_client = redis.from_url(REDIS_URL, decode_responses=True)
+
+
+
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        time.sleep(60)
+        while True:
+            time.sleep(5)
+            try:
+                gridBots = GridBot.objects.filter(status=True)
+                if not gridBots:
+                    continue
+                for gridBot in gridBots:
+                    gridBOTkey = f"GridBot {gridBot.id}"
+                    if redis_client.get(gridBOTkey):
+                        continue
+                    s=gridBot.account.checkAccount()
+                    if not s :
+                        continue
+                    print(s)
+                    redis_client.setex(gridBOTkey, gridBot.interval, 1)
+                    gridBot.checkOpenGrids()
+                    gridBot.makeOrders()
+            except Exception as e:
+                print(e)
+                print("200 SEC SLEEP EXEPTION")
+                time.sleep(200)
