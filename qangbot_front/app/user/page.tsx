b9f8f207ff00b65db2c5ffe1ebd440911f824287
4tendev <4tenprog@gmail.com>
2024-05-06 11:09:34 +0330

    show auth in case of not user

--- qangbot_front/app/user/page.tsx
+++ qangbot_front/app/user/page.tsx
@@ -1,11 +1,12 @@
-"use client"
-import React from 'react'
-import UserProfile from '../navbar/Profile/UserProfile/UserProfile'
-
+"use client";
+import React from "react";
+import UserProfile from "../navbar/Profile/UserProfile/UserProfile";
+import { isKnown } from "@/GlobalStates/Slices/userSlice";
+import Auth from "./auth/page";
+import { useAppSelector } from "@/GlobalStates/hooks";
 const Page = () => {
-  return (
-    <UserProfile />
-  )
-}
+  const userisKnown = useAppSelector(isKnown);
+  return userisKnown ? <UserProfile /> : <Auth />;
+};
 
-export default Page
+export default Page;
