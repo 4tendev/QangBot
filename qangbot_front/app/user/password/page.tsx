09a8a47a370c2e1bb7cbdce4f492cb5eb38e6236
4tendev <4tenprog@gmail.com>
2024-05-06 11:09:05 +0330

    password change

--- qangbot_front/app/user/password/page.tsx
+++ qangbot_front/app/user/password/page.tsx
@@ -1,9 +1,70 @@
-import React from 'react'
+"use client";
+import UseFormTemplate from "@/app/UseForm/UseFormTemplate";
+import React from "react";
+import {
+  passwordInput,
+  newPasswordInput,
+  repeatPasswordInput,
+} from "../auth/inputs/inputs";
+import { useAppDispatch, useAppSelector } from "@/GlobalStates/hooks";
+import responseMessageFinder from "@/app/commonTs/responseMessageFinder";
+import dictionary from "./dictionary.json";
+import { language } from "@/GlobalStates/Slices/languageSlice";
 
+import { newAlert } from "@/GlobalStates/Slices/alert/Slice";
+import { fetchapi } from "@/commonTsBrowser/fetchAPI";
+import { isKnown } from "@/GlobalStates/Slices/userSlice";
+import Auth from "../auth/page";
 const Page = () => {
-  return (
-    <div>Page</div>
-  )
-}
+  const userisKnown = useAppSelector(isKnown);
+  const lang = useAppSelector(language).lang;
+  const setGlobalState = useAppDispatch();
+  const changePasswordForm = {
+    inputs: [
+      passwordInput(lang),
+      newPasswordInput(lang),
+      repeatPasswordInput(lang),
+    ],
+    action: "change",
+  };
 
-export default Page
+  async function changePassword(data: {
+    password: string;
+    repeatPassword: string;
+    newPassword: string;
+  }) {
+    if (data.newPassword !== data.repeatPassword) {
+      setGlobalState(
+        newAlert({
+          message: dictionary.notSamePassword[lang],
+          mode: "warning",
+          time: 4,
+        })
+      );
+      return;
+    }
+    await fetchapi("/user/", "PUT", data).then((response) => {
+      const code = response.code;
+      const mode = code.substring(0, 1) === "2" ? "success" : "warning";
+
+      setGlobalState(
+        newAlert({
+          message: responseMessageFinder(dictionary, lang, code),
+          mode: mode,
+          time: 4,
+        })
+      );
+    });
+  }
+
+  return userisKnown === true ? (
+    <div className="max-w-lg mx-auto">
+      Change Password
+      <UseFormTemplate form={changePasswordForm} action={changePassword} />
+    </div>
+  ) : (
+    <Auth />
+  );
+};
+
+export default Page;
