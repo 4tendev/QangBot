33ce76be2a16a3ac3bacbd1cfa4f511d39924679
4tendev <4tenprog@gmail.com>
2024-05-10 16:44:55 +0330

    work with bot slice and reload on user relogin

--- qangbot_front/app/navbar/Roobot/Robot.tsx
+++ qangbot_front/app/navbar/Roobot/Robot.tsx
@@ -5,27 +5,29 @@ import { useState, useRef, useEffect } from "react";
 import dictionary from "./dictionary.json";
 import { fetchapi } from "@/commonTsBrowser/fetchAPI";
 import { language } from "@/GlobalStates/Slices/languageSlice";
+import { bots, newBotInfo, BotInfo } from "@/GlobalStates/Slices/botSlice";
 
-import { useAppSelector } from "@/GlobalStates/hooks";
-import { Bot } from "@/app/gridbot/types";
-
+import { useAppSelector, useAppDispatch } from "@/GlobalStates/hooks";
+import { isKnown } from "@/GlobalStates/Slices/userSlice";
 const Robot = () => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLUListElement | null>(null);
+  const dispatch = useAppDispatch();
 
   const lang = useAppSelector(language).lang;
-  const [bots, setBots]: [Bot[] | undefined, Function] = useState<
-    Bot[] | undefined
-  >(undefined);
-
-  const [error, setError] = useState(false);
-  async function getBots() {
+  const botList = useAppSelector(bots);
+  const userIsKnown =  useAppSelector(isKnown)
+  const [error, setError]: [boolean | undefined, Function] =
+    useState(undefined);
+  async function getBots(): Promise<BotInfo | void> {
+    setError(undefined);
     try {
       const response = await fetchapi("/gridbot/", "GET");
+      response.code ? setError(false) : null;
       if (response.code == "400") {
-        return [];
+        return { bots: [], canCreateBot: undefined };
       }
-      return response.data.bots as Bot[];
+      return response.data as BotInfo;
     } catch (error) {
       setError(true);
     }
@@ -51,19 +53,30 @@ const Robot = () => {
   };
 
   useEffect(() => {
-    getBots().then((response) => {
-      response ? setBots(response) : null;
-    });
+    error === undefined
+      ? getBots().then((response) => {
+          response ? dispatch(newBotInfo(response)) : null;
+        })
+      : null;
 
     return () => {};
   }, [error]);
+
+  useEffect(() => {
+    error !== undefined ?  setError (undefined) :null
+  
+    return () => {
+      
+    }
+  }, [userIsKnown])
+  
 
   const liClassName = "h-12 w-full p-0 flex items-center";
 
   return error === true ? (
     <button
       className="btn btn-warning btn-xs ms-4 "
-      onClick={() => setError(false)}
+      onClick={() => setError(undefined)}
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -74,7 +87,7 @@ const Robot = () => {
         <path d="M142.9 142.9c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5c0 0 0 0 0 0H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5c7.7-21.8 20.2-42.3 37.8-59.8zM16 312v7.6 .7V440c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l41.6-41.6c87.6 86.5 228.7 86.2 315.8-1c24.4-24.4 42.1-53.1 52.9-83.7c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.2 62.2-162.7 62.5-225.3 1L185 329c6.9-6.9 8.9-17.2 5.2-26.2s-12.5-14.8-22.2-14.8H48.4h-.7H40c-13.3 0-24 10.7-24 24z" />
       </svg>
     </button>
-  ) : bots === undefined ? (
+  ) : error === undefined ? (
     <span className="loading loading-dots loading-sm ms-5 mt-2"></span>
   ) : (
     <div className="dropdown dropdown-start ">
@@ -101,7 +114,7 @@ const Robot = () => {
           tabIndex={0}
           className="dropdown-content rounded-none z-[2] mt-4 py-1 shadow bg-base-200  w-52"
         >
-          {bots?.map((bot) => (
+          {botList?.map((bot) => (
             <li key={bot.id} className={liClassName}>
               <Link
                 onClick={handleItemClick}
