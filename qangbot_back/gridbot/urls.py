e41321010f1d15197478af2ac1b0ee5bd9e01eeb
4tendev <4tenprog@gmail.com>
2024-05-11 19:46:46 +0330

    endpoint for contracts and exchanges

--- qangbot_back/gridbot/urls.py
+++ qangbot_back/gridbot/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.bot , name = "bot"),
     path('exchange/', views.exchange , name = "exchange"),
+    path('<str:exchangeName>/', views.account , name = "account"),
+    path('<str:exchangeName>/contract/', views.contract , name = "contract"),
+
     
 ]
