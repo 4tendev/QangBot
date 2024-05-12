d01079b149d673ef79e0426fef97d4fae7b47f79
4tendev <4tenprog@gmail.com>
2024-05-12 19:34:41 +0330

    correct account endpoint

--- qangbot_back/gridbot/urls.py
+++ qangbot_back/gridbot/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.bot , name = "bot"),
     path('exchange/', views.exchange , name = "exchange"),
-    path('<str:exchangeName>/', views.account , name = "account"),
+    path('<str:exchangeName>/account/', views.account , name = "account"),
     path('<str:exchangeName>/contract/', views.contract , name = "contract"),
 
     
