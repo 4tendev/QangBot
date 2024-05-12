5a3c4f5391d5afd01a71a20be8d207d35b87a89f
4tendev <4tenprog@gmail.com>
2024-05-12 19:34:08 +0330

    coinex account form

--- qangbot_back/gridbot/forms.py
+++ qangbot_back/gridbot/forms.py
@@ -5,3 +5,8 @@ class CreateBotForm(forms.Form):
     contractID = forms.IntegerField( required=True)
     exchangeID = forms.IntegerField( required=True)
     accountID = forms.IntegerField( required=True)
+
+class CreateCoinexAccountForm(forms.Form):
+    access_ID=forms.CharField( max_length=50, required=True)
+    secret_key=forms.CharField( max_length=50, required=True)
+    name =forms.CharField( max_length=20, required=True)
