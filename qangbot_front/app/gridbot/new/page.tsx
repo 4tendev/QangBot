f2890628cf6ed6b164e2e088d444f433802c1ff9
4tendev <4tenprog@gmail.com>
2024-05-12 20:50:16 +0330

    Update page.tsx

--- qangbot_front/app/gridbot/new/page.tsx
+++ qangbot_front/app/gridbot/new/page.tsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import NewBotForm from "./NewBotForm"
-import getData from '@/commonTsServer/getData'
+import React from "react";
+import NewBotForm from "./NewBotForm";
+import getData from "@/commonTsServer/getData";
 
 const Page = async () => {
-  const exchanges= await getData("/gridbot/exchange/")
+  const exchanges = await getData("/gridbot/exchange/");
   if (exchanges.code !== "200") {
-   throw new Error('Server Error')
+    throw new Error("Server Error");
   }
-  return (
-      <NewBotForm exchanges={exchanges.data.exchanges} />  
-  )
-}
+  return <NewBotForm exchanges={exchanges.data.exchanges} />;
+};
 
-export default Page
+export default Page;
