d1cf179e55f288277a24f4dfe60757721fdbc2dc
4tendev <4tenprog@gmail.com>
2024-05-13 14:22:35 +0330

    Update urls.py

--- qangbot_back/gridbot/urls.py
+++ qangbot_back/gridbot/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path('', views.bot , name = "bot"),
-    path('exchange/', views.exchange , name = "exchange"),
-    path('<str:exchangeName>/account/', views.account , name = "account"),
-    path('<str:exchangeName>/contract/', views.contract , name = "contract"),
-
+    path('', views.gridbots , name = "gridbots"),
+    path('exchange/', views.exchanges , name = "exchanges"),
+    path('<str:exchangeName>/account/', views.accounts , name = "accounts"),
+    path('<str:exchangeName>/contract/', views.contracts , name = "contracts"),
+    path('<int:id>/', views.gridbot , name = "gridbot"),
     
 ]
