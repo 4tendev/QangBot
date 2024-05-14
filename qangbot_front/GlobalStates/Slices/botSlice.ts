1e1c00e55a71e5ef12dac6e31254482be9f9f3d9
4tendev <4tenprog@gmail.com>
2024-05-14 18:18:43 +0330

    Update botSlice.ts

--- qangbot_front/GlobalStates/Slices/botSlice.ts
+++ qangbot_front/GlobalStates/Slices/botSlice.ts
@@ -1,30 +1,40 @@
 import type { PayloadAction } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 import { BotList, Bot } from "@/app/gridbot/types";
-export type BotInfo = { bots: BotList; canCreateBot: boolean | undefined };
-const initialState: BotInfo = { bots: [], canCreateBot: undefined };
+export type BotInfo = {
+  bots: BotList;
+  canCreateBot: boolean | undefined;
+  isLoaded: boolean;
+};
+const initialState: BotInfo = {
+  bots: [],
+  canCreateBot: undefined,
+  isLoaded: false,
+};
 
 export const botSlice = createSlice({
   name: "botInfo",
   initialState,
   reducers: (create) => ({
-    setBotList: create.reducer((state, action: PayloadAction<BotList>) => {
-      state.bots = action.payload;
-    }),
     newBotInfo: create.reducer((state, action: PayloadAction<BotInfo>) => {
       return action.payload;
     }),
-    addBot: create.reducer((state, action: PayloadAction<Bot>) => {
-      state.bots = [...state.bots, action.payload];
+
+    updateBot: create.reducer((state, action: PayloadAction<Bot>) => {
+      const oldstate = [...state.bots].filter(
+        (bot) => bot.id !== action.payload.id
+      );
+      state.bots = [...oldstate, action.payload];
     }),
   }),
 
   selectors: {
+    botInfo: (botInfo) => botInfo,
     bots: (botInfo) => botInfo.bots,
     canAddBot: (botInfo) => botInfo.canCreateBot,
   },
 });
 
-export const { setBotList, newBotInfo, addBot } = botSlice.actions;
+export const { newBotInfo, updateBot } = botSlice.actions;
 
-export const { bots, canAddBot } = botSlice.selectors;
+export const { bots, canAddBot, botInfo } = botSlice.selectors;
