e5e1e0d04fb4f44681d5f29eaab7e6ad3bbdfd82
4tendev <4tenprog@gmail.com>
2024-05-15 13:36:57 +0330

    update notfound

--- qangbot_front/app/not-found.jsx
+++ qangbot_front/app/not-found.jsx
@@ -1,8 +1,13 @@
+"use client"
+import { language } from '@/GlobalStates/Slices/languageSlice'
+import { useAppSelector } from '@/GlobalStates/hooks'
 import React from 'react'
+import dictionary from "./dictionary.json"
 
 const Page = () => {
+  const lang=useAppSelector(language).lang
   return (
-    <div className='text-center w-full' > Not Found </div>
+    <div className='text-center w-full' >{dictionary.notFound[lang]}</div>
   )
 }
 
