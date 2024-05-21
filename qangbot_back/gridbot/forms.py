39a9cbed9fdb8582533635c73356d9b63898160c
4tendev <4tenprog@gmail.com>
2024-05-21 15:47:09 +0330

    Update forms.py

--- qangbot_back/gridbot/forms.py
+++ qangbot_back/gridbot/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 class CreateBotForm(forms.Form):
     name = forms.CharField( max_length=20, required=True)
@@ -18,3 +19,22 @@ class BotActions(forms.Form):
         ("resume", "Resume"),
     ]
     action=forms.ChoiceField( choices=CHOICES, required=True)
+
+
+def grids_validator(value):
+    required_keys=["sell" , "buy" , "nextPosition","size" ] 
+    for grid in value :
+        keys = grid.keys()
+        for key in required_keys:
+                if key not in keys:
+                    raise ValidationError(f"The '{key}' is required.")
+    for grid in value :
+        for key in required_keys :
+            try :
+                if not float(grid[key])  >0 :
+                    raise ValidationError(f"The '{key}' is not POSITIVE.")
+            except :
+                raise ValidationError(f"The '{key}' is not POSITIVE.")
+
+class CreateGridsForm(forms.Form):
+    grids=forms.JSONField(required=True , validators=[grids_validator])    
