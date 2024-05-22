285b127da355933fae01754ebfa2c19645e72bb4
4tendev <4tenprog@gmail.com>
2024-05-22 15:43:01 +0330

    change default creation from app to gridbot script

--- qangbot_back/gridbot/apps.py
+++ qangbot_back/gridbot/apps.py
@@ -5,36 +5,4 @@ class GridbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gridbot'
 
-    def ready(self):
-        super().ready()
-        from .models import Exchange, CoinexAccount, Contract
-        from django.contrib.contenttypes.models import ContentType
-        from django.db.models.signals import post_migrate
-        from django.dispatch import receiver
 
-        @receiver(post_migrate)
-        def create_default_exchange(sender, **kwargs):
-            if sender.name == 'gridbot':
-                coinexFutureExchange = Exchange.objects.get_or_create(
-                    name="Coinex Future", account_model=ContentType.objects.get_for_model(CoinexAccount))
-                CoinexFutureContracts = [
-                    {
-                        "name": "BTCUSDT",
-                        "url": "https://www.coinex.com/en/futures/marketinfo/info?market=BTCUSDT",
-                        "apiIdentifier": "BTCUSDT"
-                    },
-                    {
-                        "name": "BTCUSD",
-                        "url": "https://www.coinex.com/en/futures/marketinfo/info?market=BTCUSD",
-                        "apiIdentifier": "BTCUSD"
-                    },
-                    {
-                        "name": "ETHUSD",
-                        "url": "https://www.coinex.com/en/futures/marketinfo/info?market=ETHUSD",
-                        "apiIdentifier": "ETHUSD"
-                    }
-
-                ]
-                for contract in CoinexFutureContracts:
-                    Contract.objects.get_or_create(
-                        exchange=coinexFutureExchange[0], url=contract["url"], name=contract["name"], apiIdentifier=contract["apiIdentifier"])
--- qangbot_back/gridbot/management/commands/gridbots.py
+++ qangbot_back/gridbot/management/commands/gridbots.py
@@ -1,15 +1,39 @@
 from django.core.management.base import BaseCommand
 import time
 import redis
-from gridbot.models import GridBot
+from gridbot.models import GridBot ,Exchange ,ContentType ,CoinexAccount ,Contract
 from core.settings import REDIS_URL
 
 redis_client = redis.from_url(REDIS_URL, decode_responses=True)
+def create_default_exchange():
+                coinexFutureExchange = Exchange.objects.get_or_create(
+                    name="Coinex Future", account_model=ContentType.objects.get_for_model(CoinexAccount))
+                CoinexFutureContracts = [
+                    {
+                        "name": "BTCUSDT",
+                        "url": "https://www.coinex.com/en/futures/marketinfo/info?market=BTCUSDT",
+                        "apiIdentifier": "BTCUSDT"
+                    },
+                    {
+                        "name": "BTCUSD",
+                        "url": "https://www.coinex.com/en/futures/marketinfo/info?market=BTCUSD",
+                        "apiIdentifier": "BTCUSD"
+                    },
+                    {
+                        "name": "ETHUSD",
+                        "url": "https://www.coinex.com/en/futures/marketinfo/info?market=ETHUSD",
+                        "apiIdentifier": "ETHUSD"
+                    }
 
+                ]
+                for contract in CoinexFutureContracts:
+                    Contract.objects.get_or_create(
+                        exchange=coinexFutureExchange[0], url=contract["url"], name=contract["name"], apiIdentifier=contract["apiIdentifier"])
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         time.sleep(60)
+        create_default_exchange()
         while True:
             time.sleep(5)
             try:
