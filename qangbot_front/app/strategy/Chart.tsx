0e42250ec758752497258538c37b5e346d4c0e9c
4tendev <4tenprog@gmail.com>
2024-05-28 17:19:03 +0330

    change part of page

--- qangbot_front/app/strategy/Chart.tsx
+++ qangbot_front/app/strategy/Chart.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import dictionary from "./dictionary.json";
 import {
   LineChart,
@@ -10,9 +9,10 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
-import { Language } from "@/settings";
 import { useEffect, useState } from "react";
 import { History } from "./types";
+import { useAppSelector } from "@/GlobalStates/hooks";
+import { language } from "@/GlobalStates/Slices/languageSlice";
 const monthNames = [
   "January",
   "February",
@@ -35,16 +35,16 @@ function formatDate(dateString: string) {
   return formattedDate;
 }
 
-const Chart = (props: { data: History[]; lang: Language }) => {
+const Chart = (props: { data: History[] }) => {
   const data = props.data.map((history: any) => {
     return { ...history, date: formatDate(history.date).substring(0, 6) };
   });
-  const lang = props.lang;
+  const lang = useAppSelector(language).lang;
   const [elementWidth, setElementWidth] = useState<number>(0);
 
   useEffect(() => {
     const updateElementWidth = () => {
-      const element = document.getElementById("chart"); // Replace with the actual ID of your element
+      const element = document.getElementById("chart");
       if (element) {
         const width = element.offsetWidth;
         setElementWidth(width);
@@ -57,111 +57,59 @@ const Chart = (props: { data: History[]; lang: Language }) => {
     };
   }, []);
   return (
-    <div className="w-full max-w-6xl mx-auto felx flex-col justify-start">
-      <div id="chart" className="w-full max-w-5xl mx-auto  py-5">
-        <div className="sm:text-lg text-info mb-4 ps-8">
-          {dictionary.strategyFrom[lang]} {data[0].date}
-        </div>
-        <div className="ps-2">
-          <LineChart
-            width={elementWidth - 20}
-            height={400}
-            data={data}
-            margin={{
-              top: 20,
-              right: 5,
-              left: -20,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 10" />
-            <XAxis dataKey="date" tickMargin={3} />
-            <YAxis
-              tickMargin={15}
-              orientation={"left"}
-              yAxisId="date"
-              tickCount={3}
-            />
-
-            <Tooltip
-              animationDuration={100}
-              allowEscapeViewBox={{ x: false }}
-            />
-            <Legend />
-
-            <Line
-              yAxisId="date"
-              type="monotone"
-              dataKey="btcROI"
-              stroke="orange"
-              dot={false}
-              strokeWidth={2}
-            />
-            <Line
-              yAxisId="date"
-              type="monotone"
-              dataKey="usdROI"
-              stroke="green"
-              strokeWidth={5}
-              dot={false}
-            />
-            <Line
-              strokeWidth={2}
-              dot={false}
-              yAxisId="date"
-              type="monotone"
-              dataKey="ethROI"
-              stroke="red"
-            />
-          </LineChart>
-        </div>
+    <div id="chart" className="w-full max-w-5xl mx-auto  py-5">
+      <div className="sm:text-lg text-info mb-4 ps-8">
+        {dictionary.strategyFrom[lang]} {data[0].date}
       </div>
-      <div className="w-full px-6 sm:px-11">
-        <div className="flex w-full justify-between">
-          <div className="flex  flex-col justify-center ">
-            <div className="text-accent text-lg font-bold">
-              {dictionary.QANGStrategy[lang]}
-            </div>
-            <div className="flex flex-wrap justify-between gap-y-6 gap-x-10 ps-3 py-3  max-w-2xl">
-              <div>
-                <h3 className="text-primary">{dictionary.assumptions[lang]}</h3>
+      <div className="ps-2">
+        <LineChart
+          width={elementWidth - 20}
+          height={400}
+          data={data}
+          margin={{
+            top: 20,
+            right: 5,
+            left: -20,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 10" />
+          <XAxis dataKey="date" tickMargin={3} />
+          <YAxis
+            tickMargin={15}
+            orientation={"left"}
+            yAxisId="date"
+            tickCount={3}
+          />
 
-                <ul className="ps-2">
-                  <li>{dictionary.adoption[lang]}</li>
-                  <li>{dictionary.cycle[lang]} </li>
-                  <li>{dictionary.volatile[lang]} </li>
-                </ul>
-              </div>
-              <div>
-                <h3 className="text-primary">
-                  {dictionary.considerations[lang]}
-                </h3>
+          <Tooltip animationDuration={100} allowEscapeViewBox={{ x: false }} />
+          <Legend />
 
-                <ul className="ps-2">
-                  <li>{dictionary.averages[lang]}</li>
-                  <li>{dictionary.log[lang]} </li>
-                  <li>{dictionary.hype[lang]}</li>
-                </ul>
-              </div>
-              <div>
-                <h3 className="text-primary">{dictionary.risk[lang]}</h3>
-
-                <ul className="ps-2">
-                  <li>{dictionary.exchange[lang]}</li>
-                  <li>{dictionary.cryptocurrency[lang]}</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="flex items-center max-[835px]:hidden  justify-center grow">
-            <Image
-              alt="Risk"
-              width={300}
-              height={300}
-              src={"/risk.png"}
-            ></Image>
-          </div>
-        </div>
+          <Line
+            yAxisId="date"
+            type="monotone"
+            dataKey="btcROI"
+            stroke="orange"
+            dot={false}
+            strokeWidth={2}
+          />
+          <Line
+            yAxisId="date"
+            type="monotone"
+            dataKey="usdROI"
+            stroke="green"
+            strokeWidth={5}
+            dot={false}
+          />
+          <Line
+            strokeWidth={2}
+            dot={false}
+            yAxisId="date"
+            type="monotone"
+            dataKey="ethROI"
+            stroke="red"
+          />
+        </LineChart>
       </div>
     </div>
   );
--- qangbot_front/app/strategy/page.tsx
+++ qangbot_front/app/strategy/page.tsx
@@ -1,18 +1,68 @@
-import React from 'react'
-import Chart from './Chart'
-import getLanguage from '@/commonTsServer/getLanguage'
-import getData from '@/commonTsServer/getData'
+import React from "react";
+import Chart from "./Chart";
+import getLanguage from "@/commonTsServer/getLanguage";
+import getData from "@/commonTsServer/getData";
+import Image from "next/image";
+import dictionary from "./dictionary.json";
 
-const page = async() => {
-  const history = await getData("/strategy/")
-  
-  const data = history.data
+const page = async () => {
+  const history = await getData("/strategy/");
 
+  const data = history.data;
 
-  const lang=getLanguage().lang
+  const lang = getLanguage().lang;
   return (
-    <Chart lang={lang}  data ={data} />
-  )
-}
+    <div className="w-full max-w-6xl mx-auto felx flex-col justify-start">
+      <Chart data={data} />
+      <div className="w-full px-6 sm:px-11">
+        <div className="flex w-full justify-between">
+          <div className="flex  flex-col justify-center ">
+            <div className="text-accent text-lg font-bold">
+              {dictionary.QANGStrategy[lang]}
+            </div>
+            <div className="flex flex-wrap justify-between gap-y-6 gap-x-10 ps-3 py-3  max-w-2xl">
+              <div>
+                <h3 className="text-primary">{dictionary.assumptions[lang]}</h3>
 
-export default page
+                <ul className="ps-2">
+                  <li>{dictionary.adoption[lang]}</li>
+                  <li>{dictionary.cycle[lang]} </li>
+                  <li>{dictionary.volatile[lang]} </li>
+                </ul>
+              </div>
+              <div>
+                <h3 className="text-primary">
+                  {dictionary.considerations[lang]}
+                </h3>
+
+                <ul className="ps-2">
+                  <li>{dictionary.averages[lang]}</li>
+                  <li>{dictionary.log[lang]} </li>
+                  <li>{dictionary.hype[lang]}</li>
+                </ul>
+              </div>
+              <div>
+                <h3 className="text-primary">{dictionary.risk[lang]}</h3>
+
+                <ul className="ps-2">
+                  <li>{dictionary.exchange[lang]}</li>
+                  <li>{dictionary.cryptocurrency[lang]}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center max-[835px]:hidden  justify-center grow">
+            <Image
+              alt="Risk"
+              width={300}
+              height={300}
+              src={"/risk.png"}
+            ></Image>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
