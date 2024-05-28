743737b2c5aa59a27b206135e1511b2885e3fdee
4tendev <4tenprog@gmail.com>
2024-05-28 11:56:49 +0330

    endpoint to show history data

--- qangbot_back/strategy/views.py
+++ qangbot_back/strategy/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Strategy, History
 
-# Create your views here.
+
+def getHistoryDate(history: History):
+    return {
+        "id": history.id,
+        "btcROI": history.btcROI,
+        "ethROI": history.ethROI,
+        "usdROI": history.usdROI,
+        "date":  history.date
+    }
+
+
+def history(request):
+    Strategy.objects.all()[0].Histories.all()
+    histrories = Strategy.objects.all()[0].Histories.all().order_by("date")
+    data = {"code": "200",
+            "data": [getHistoryDate(history) for history in histrories
+                     ]if histrories else []
+            }
+    return JsonResponse(data=data)
