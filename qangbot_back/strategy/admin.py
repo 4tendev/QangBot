6c18e1725532e293da96d5801ac1d1fd36f01afa
4tendev <4tenprog@gmail.com>
2024-05-28 11:56:00 +0330

    add history to admin

--- qangbot_back/strategy/admin.py
+++ qangbot_back/strategy/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Account ,Asset ,Strategy,AssetValue  ,CoinexFutureAccount
+from .models import Account ,Asset ,Strategy,AssetValue  ,CoinexFutureAccount ,History
 
 admin.site.register(Account)
 admin.site.register(CoinexFutureAccount)
+admin.site.register(History)
 
 admin.site.register(Asset)
 admin.site.register(Strategy)
