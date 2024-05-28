6d48ea9dfdda2eb21c2251a191a68c82444a2f36
4tendev <4tenprog@gmail.com>
2024-05-28 11:56:15 +0330

    history updater for strategy

---
+++ qangbot_back/strategy/management/commands/updateHistory.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand
+import time
+from strategy.models import Strategy, Asset, asstUSDRate, History
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        time.sleep(1)
+        try:
+            strategies = Strategy.objects.all()
+            if strategies:
+                for strategy in strategies:
+                    currentUSDValue = strategy.currentUSDValue()
+
+                    currentETHValue = currentUSDValue / asstUSDRate("ETH")
+                    time.sleep(5)
+                    currentBTCValue = currentUSDValue / asstUSDRate("BTC")
+                    time.sleep(5)
+
+                    baseUSD, baseETH, baseBTC = strategy.baseAssetValues.get(
+                        asset__name="USD").amount, strategy.baseAssetValues.get(
+                        asset__name="ETH").amount, strategy.baseAssetValues.get(asset__name="BTC").amount
+
+                    today_date = timezone.now().today().date()
+
+                    USDROI = round(100*((currentUSDValue/baseUSD) - 1), 0)
+                    ETHROI = round(100*((currentETHValue/baseETH) - 1), 0)
+                    BTCROI = round(100*((currentBTCValue/baseBTC) - 1), 0)
+
+                    history = History.objects.filter(date=today_date)
+                    if history:
+                        history = history[0]
+                        history.ethROI = ETHROI
+                        history.btcROI = BTCROI
+                        history.usdROI = USDROI
+                        history.strategy = strategy
+                        history.save()
+                    else:
+                        History.objects.create(
+                            ethROI=ETHROI,date=today_date , btcROI=BTCROI, usdROI=USDROI, strategy=strategy)
+
+        except Exception as e:
+            print(e)
+        print("OK STRATEGT script")
