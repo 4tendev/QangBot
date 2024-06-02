7cc21781f45f20c241d97b67f68de464156cebd0
4tendev <4tenprog@gmail.com>
2024-06-02 15:34:39 +0330

    ui

--- qangbot_front/app/Index/Invest.tsx
+++ qangbot_front/app/Index/Invest.tsx
@@ -6,7 +6,7 @@ import getLanguage from "@/commonTsServer/getLanguage";
 const Invest = () => {
   const lang = getLanguage().lang;
   return (
-    <div className="text-start  gap-x-2 w-full items-center justify-center flex sm:flex-col gorow px-9 mt-5">
+    <div className="text-start  gap-x-2 w-full items-center justify-center flex sm:flex-col gorow px-9 my-5 pb-2">
       <div className="">
         <h4 className="text-[22vw] md:text-[160px] font-bold py-0 text-error leading-none w-fit">
           {dictionary.risk[lang]}
@@ -18,13 +18,13 @@ const Invest = () => {
       <div className="flex gap-2 items-center justify-center md:mt-[20px] mt-[3vw]  flex-col sm:flex-row ">
         <Link
           href={"/invest"}
-          className="btn btn-xs btn-neutral max-w-[135px]  sm:grow "
+          className="btn btn-xs btn-neutral max-w-[135px]  sm:grow  sm:min-w-40"
         >
           {dictionary.performance[lang]}
         </Link>
         <Link
           href={"/invest/participant"}
-          className="btn btn-xs btn-primary max-w-[135px] shadow-2xl sm:grow"
+          className="btn btn-xs btn-primary max-w-[135px] shadow-2xl sm:grow sm:min-w-40"
         >
           {dictionary.participate[lang]}
         </Link>
