7f6a69a3027e48dbde8f39268e352c69e240a88b
4tendev <4tenprog@gmail.com>
2024-06-17 13:45:04 +0330

    no change

--- qangbot_front/app/Main.tsx
+++ qangbot_front/app/Main.tsx
@@ -1,29 +1,30 @@
-"use client"
-import { NAVBAR_ID } from '@/settings';
-import React, { useEffect } from 'react';
+"use client";
+import { NAVBAR_ID } from "@/settings";
+import React, { useEffect } from "react";
 
 function Main({ children }: { children: React.ReactNode }) {
-    useEffect(() => {
-       
-        const navbarElement  :HTMLElement | null = document.getElementById(NAVBAR_ID);
-        const mainElement :HTMLElement | null = document.querySelector('.mainContent');
-        const NavbarHeight = navbarElement?.offsetHeight || 80
-        if (mainElement) {
-          mainElement.style.height = (window.innerHeight -NavbarHeight) + 'px';
-        }
-        
-        function handleResize() {
-          if (mainElement) {
-            mainElement.style.height = (window.innerHeight - NavbarHeight) + 'px';
-          }
-        }
-    
-        window.addEventListener('resize', handleResize);
-        
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-      }, []);
+  useEffect(() => {
+    const navbarElement: HTMLElement | null =
+      document.getElementById(NAVBAR_ID);
+    const mainElement: HTMLElement | null =
+      document.querySelector(".mainContent");
+    const NavbarHeight = navbarElement?.offsetHeight || 80;
+    if (mainElement) {
+      mainElement.style.height = window.innerHeight - NavbarHeight + "px";
+    }
+
+    function handleResize() {
+      if (mainElement) {
+        mainElement.style.height = window.innerHeight - NavbarHeight + "px";
+      }
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <main className="w-full max-w-6xl mx-auto   mainContent overflow-auto bg-base-100">
       {children}
