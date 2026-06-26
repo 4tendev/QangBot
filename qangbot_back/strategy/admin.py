fb6a902c1fedeaa6aa31c14ee04d6c0c71852cbe
4tendev <4tenprog@gmail.com>
2026-06-27 00:06:45 +0330

    Update admin.py

--- qangbot_back/strategy/admin.py
+++ qangbot_back/strategy/admin.py
@@ -1,13 +1,32 @@
 from django.contrib import admin
 from .models import Transaction, Account, Asset, Strategy, AssetValue, CoinexFutureAccount, History, Participant, ParticipantBTCAddress,LyraAccount,Withdraw
 
+
+
+from collections import defaultdict
+@admin.register(Strategy)
+class StrategyAdmin(admin.ModelAdmin):
+    list_display = ("id", "asset_summary")
+
+    def asset_summary(self, obj):
+        totals = defaultdict(float)
+
+        for value in obj.currentAssetValues.select_related("asset"):
+            totals[value.asset.name] += value.amount
+
+        return ", ".join(
+            f"{asset}: {amount}"
+            for asset, amount in totals.items()
+        )
+
+    asset_summary.short_description = "Current Assets"
+
 admin.site.register(Account)
 admin.site.register(Transaction)
 admin.site.register(CoinexFutureAccount)
 admin.site.register(History)
 admin.site.register(Participant)
 admin.site.register(Asset)
-admin.site.register(Strategy)
 admin.site.register(AssetValue)
 admin.site.register(LyraAccount)
 admin.site.register(ParticipantBTCAddress)
